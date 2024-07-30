8eb1494e7264202000780a29002facf7034fb1ed
Colin <Colin.Reardon@triosstudent.com>
2024-07-29 23:08:37 -0400

    Made Start screen and Credit Screen

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{ 
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
